ae0b0380444854c2ecebaf3a18439fb76f05bfdd
Christoph Hoeller <c.hoeller@beyond-interfaces.com>
2020-12-02 17:13:28 +0100

    Babywatch NG11

--- babywatch/src/app/delete-timeline-dialog/delete-timeline-dialog.component.spec.ts
+++ babywatch/src/app/delete-timeline-dialog/delete-timeline-dialog.component.spec.ts
@@ -1,4 +1,4 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import {DeleteTimelineDialogComponent} from './delete-timeline-dialog.component';
 
@@ -6,7 +6,7 @@ describe('DeleteTimelineDialogComponent', () => {
   let component: DeleteTimelineDialogComponent;
   let fixture: ComponentFixture<DeleteTimelineDialogComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [DeleteTimelineDialogComponent]
     })
