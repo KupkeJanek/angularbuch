742be0eeae0ecf254af2554bb76c6b7f93c97f97
Christoph Höller <christoph.hoeller@gmail.com>
2021-08-21 13:14:21 +0200

    fix app.module ssr

--- project-manager-ssr/src/app/app.module.ts
+++ project-manager-ssr/src/app/app.module.ts
@@ -1,23 +1,18 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule, BrowserTransferStateModule, Title} from '@angular/platform-browser';
-
-import {AppComponent} from './app.component';
-import {LoginService} from './services/login-service/login-service';
-import {appRouting, routingComponents} from './app.routing';
+import { NgModule } from '@angular/core';
+import { BrowserModule, BrowserTransferStateModule, Title } from '@angular/platform-browser';
 import * as io from 'socket.io-client';
-import {AUTH_ENABLED, SOCKET_IO} from './app.tokens';
-import {environment} from '../environments/environment';
-import {mockIO} from './mocks/mock-socket';
-import {SharedModule} from './shared/shared-module';
+import { AppComponent } from './app.component';
+import { appRouting, routingComponents } from './app.routing';
+import { AUTH_ENABLED, SOCKET_IO } from './app.tokens';
+import { LoginService } from './services/login-service/login-service';
+import { SharedModule } from './shared/shared-module';
+
 
 export function socketIoFactory() {
-  if (environment.e2eMode) {
-    return mockIO;
-  }
   return io;
 }
 
-const enableAuthentication = !environment.e2eMode;
+const enableAuthentication = false;
 
 @NgModule({
   imports: [BrowserModule.withServerTransition({ appId: 'serverApp' }),     BrowserTransferStateModule,
