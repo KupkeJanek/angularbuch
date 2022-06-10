7db900fee5873e060bc822e33c5abf55a1be1680
Janek Kupke <38261186+KupkeJanek@users.noreply.github.com>
2022-06-10 08:45:01 +0200

    removed app.module

--- project-manager-modular/src/main.ts
+++ project-manager-modular/src/main.ts
@@ -1,9 +1,9 @@
 import { APP_INITIALIZER, enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication, Title } from '@angular/platform-browser';
 import { AppComponent, appRouting } from './app';
-import { enableAuthentication, initializeApplication, socketIoFactory } from './app/app.module';
 import { AUTH_ENABLED, SOCKET_IO } from './app/app.tokens';
 import { CacheModule } from './app/cache/cache.module';
+import { mockIO } from './app/mocks/mock-socket';
 import { ApplicationConfigService } from './app/services/application-config/application-config.service';
 import { LoginService } from './app/services/login-service/login-service';
 import { SharedModule } from './app/shared/shared-module';
@@ -13,6 +13,20 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
+
+export function socketIoFactory() {
+  if (environment.e2eMode) {
+    return mockIO;
+  }
+  return io;
+}
+
+export function initializeApplication(applicationConfigService: ApplicationConfigService) {
+  return () => applicationConfigService.loadConfig();
+}
+
+const enableAuthentication = !environment.e2eMode;
+
 
 bootstrapApplication(AppComponent, {
   providers: [
